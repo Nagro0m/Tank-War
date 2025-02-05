03b256bd4a928630f22ff1b1e19c48af3cf2df75
Nagrom <93036733+Nagro0m@users.noreply.github.com>
2025-02-05 17:00:05 +0100

    start fixing issues

--- Level.cpp
+++ Level.cpp
@@ -11,34 +11,34 @@ void Level::GenerateLevel()
 	LevelGenerator _generator = LevelGenerator(this, 3840, 2160, 106);
 	//LevelGenerator _generator = LevelGenerator(this, 1920, 1080, 106);
 
-	vector<LevelElement*> _groundVariants;
-	LevelElement* _ground = new LevelElement(new MeshActor(RectangleShapeData({ 180.0f, 180.0f }, "Object/Grounds_3")), _groundVariants);
+	vector<LevelElement<MeshActor>*> _groundVariants;
+	LevelElement<MeshActor>* _ground = new LevelElement(new MeshActor(RectangleShapeData({ 180.0f, 180.0f }, "Object/Grounds_3")), _groundVariants);
 	_generator.SetGround(_ground, { 120, 120 });
 
-	vector<LevelElement*> _treeVariants;
+	vector<LevelElement<Tree>*> _treeVariants;
 	const Vector2f& _size = Vector2f(80.0f, 80.0f);
 	_treeVariants.push_back(new LevelElement(new Tree(RectangleShapeData(_size, "Object/Tree_4")), 2));
 	_treeVariants.push_back(new LevelElement(new Tree(RectangleShapeData(_size, "Object/Tree_3")), 2));
 	_treeVariants.push_back(new LevelElement(new Tree(RectangleShapeData(_size, "Object/Tree_2")), 2));
-	LevelElement* _tree = new LevelElement(new Tree(RectangleShapeData(_size, "Object/Tree_1")), 10, _treeVariants);
+	LevelElement<Tree>* _tree = new LevelElement(new Tree(RectangleShapeData(_size, "Object/Tree_1")), 10, _treeVariants);
 
-	vector<LevelElement*> _grassVariants;
+	vector<LevelElement<Grass>*> _grassVariants;
 	const Vector2f& _grassSize = Vector2f(30.0f, 30.0f);
 	_grassVariants.push_back(new LevelElement(new Grass(RectangleShapeData(_grassSize, "Object/Grass_2")), 2));
-	LevelElement* _grass = new LevelElement(new Grass(RectangleShapeData(_grassSize, "Object/Grass_1")), 5, _grassVariants);
+	LevelElement<Grass>* _grass = new LevelElement(new Grass(RectangleShapeData(_grassSize, "Object/Grass_1")), 5, _grassVariants);
 			
 
-	vector<LevelElement*> _rootVariants;
+	vector<LevelElement<Root>*> _rootVariants;
 	const Vector2f& _rootSize = Vector2f(50.0f, 30.0f);
 	_rootVariants.push_back(new LevelElement(new Root(RectangleShapeData(_rootSize, "Object/Root_2")), 2));
-	LevelElement* _root = new LevelElement(new Root(RectangleShapeData(_rootSize, "Object/Root_1")), 1, _rootVariants);
+	LevelElement<Root>* _root = new LevelElement(new Root(RectangleShapeData(_rootSize, "Object/Root_1")), 1, _rootVariants);
 
-	vector<LevelElement*> _rockVariants;
+	vector<LevelElement<Rock>*> _rockVariants;
 	const Vector2f& _rockSize = Vector2f(50.0f, 40.0f);
 	_rockVariants.push_back(new LevelElement(new Rock(RectangleShapeData(_rockSize, "Object/Rock_1")), 1));
 	_rockVariants.push_back(new LevelElement(new Rock(RectangleShapeData(_rockSize, "Object/Rock_2")), 1));
 	_rockVariants.push_back(new LevelElement(new Rock(RectangleShapeData(_rockSize, "Object/Rock_3")), 1));
-	LevelElement* _rock = new LevelElement(new Rock(RectangleShapeData(_rockSize, "Object/Rock_4")), 3, _rockVariants);
+	LevelElement<Rock>* _rock = new LevelElement(new Rock(RectangleShapeData(_rockSize, "Object/Rock_4")), 3, _rockVariants);
 
 	_generator.AddGenerableElement(_tree);
 	_generator.AddGenerableElement(_rock);
--- LevelGenerator.h
+++ LevelGenerator.h
@@ -2,11 +2,14 @@
 
 #include "CoreMinimal.h"
 #include "MeshActor.h"
+#include "Level.h"
+#include "GameManager.h"
+#include "CollisionComponent.h"
 
 class Level;
 enum CollisionType;
 
-template<typename T>
+template<typename T = MeshActor>
 class LevelElement
 {
 	int chance;
@@ -30,14 +33,38 @@ public:
 
 public:
 #pragma region Constructors
-	LevelElement(T* _actor, const int _chance);
-	LevelElement(T* _actor, const int _chance, const vector<LevelElement*>& _variants);
+	LevelElement(T* _actor, const int _chance)
+	{
+		prefab = _actor;
+		chance = _chance;
+		variants = vector<LevelElement*>();
+	}
+
+	LevelElement(T* _actor, const int _chance, const vector<LevelElement*>& _variants)
+	{
+		prefab = _actor;
+		chance = _chance;
+		variants = _variants;
+	}
 	/// <summary>
 	/// Constructeur pour créer un LevelElement qui servira de sol
 	/// </summary>
-	LevelElement(MeshActor* _actor, const vector<LevelElement*>& _variants=vector<LevelElement*>());
+	LevelElement(MeshActor* _actor, const vector<LevelElement*>& _variants=vector<LevelElement*>())
+	{
+		prefab = _actor;
+		chance = 0;
+		variants = _variants;
+	}
+
 	LevelElement() = default;
-	~LevelElement();
+	~LevelElement()
+	{
+		for (LevelElement* _variant : variants)
+		{
+			delete _variant;
+		}
+		delete prefab;
+	}
 #pragma endregion
 
 public:
@@ -45,15 +72,19 @@ public:
 	{
 		return variants.size() > 0;
 	}
-	LevelElement<T>* GetRandomVariant(); // Pas de const, ça bloque le return this
+	LevelElement<T>* GetRandomVariant() // Pas de const, ça bloque le return this
+	{
+		LevelElement* _element = LevelGenerator<T>::GetRandomElement(variants);
+		return _element ? _element : this;
+	}
 
 };
-
+template<typename T = MeshActor>
 class LevelGenerator
 {
 	u_int sizeX, sizeY;
 	vector<MeshActor*> generatedElements;
-	vector<LevelElement*> elementsList;
+	vector<LevelElement<T>*> elementsList;
 	Level* level;
 	u_int spaceBetweenElements;
 
@@ -69,19 +100,67 @@ public:
 
 public:
 #pragma region Constructors
-	LevelGenerator(Level* _level, const u_int& _sizeX, const u_int& _sizeY, const u_int& _spaceBetweenElements);
-	LevelGenerator(Level* _level, const u_int& _sizeX, const u_int& _sizeY, const vector<LevelElement*>& _elementsList, const u_int& _spaceBetweenElements);
-	~LevelGenerator();
+	LevelGenerator(Level* _level, const u_int& _sizeX, const u_int& _sizeY, const u_int& _spaceBetweenElements)
+	{
+		level = _level;
+		sizeX = _sizeX;
+		sizeY = _sizeY;
+		elementsList = vector<LevelElement*>();
+		spaceBetweenElements = _spaceBetweenElements;
+		ground = nullptr;
+
+	}
+
+	LevelGenerator(Level* _level, const u_int& _sizeX, const u_int& _sizeY, const vector<LevelElement<T>*>& _elementsList, const u_int& _spaceBetweenElements)
+	{
+		level = _level;
+		sizeX = _sizeX;
+		sizeY = _sizeY;
+		elementsList = _elementsList;
+		spaceBetweenElements = _spaceBetweenElements;
+		ground = nullptr;
+	}
+	~LevelGenerator()
+	{
+		for (LevelElement* _element : elementsList)
+		{
+			delete _element;
+		}
+		delete ground;
+	}
 #pragma endregion
 
 #pragma region Methods
 public:
-	FORCEINLINE void AddGenerableElement(LevelElement* _element)
+	FORCEINLINE void AddGenerableElement(LevelElement<T>* _element)
 	{
 		elementsList.push_back(_element);
 	}
 
-	map<Vector2f, MeshActor*> GenerateMap();
+	map<Vector2f, MeshActor*> GenerateMap()
+	{
+		map<Vector2f, MeshActor*> _map;
+
+		const float _offset = 40.0f;
+
+		GenerateGround();
+
+		for (u_int _y = 0; _y < sizeY; _y += spaceBetweenElements)
+		{
+			if (_y + spaceBetweenElements >= sizeY) continue;
+
+			for (u_int _x = 0; _x < sizeX; _x += spaceBetweenElements)
+			{
+				if (_x + spaceBetweenElements >= sizeX) continue;
+
+				const float _offsetX = GetRandomNumberInRange<float>(-_offset, _offset);
+				const float _offsetY = GetRandomNumberInRange<float>(-_offset, _offset);
+				const Vector2f& _position = Vector2f((float)_x + _offsetX, (float)_y + _offsetY);
+				GenerateRandomElement(_position);
+			}
+		}
+		return _map;
+	}
 
 private:
 	/// <summary>
@@ -89,11 +168,90 @@ private:
 	/// </summary>
 	/// <param name="_pos">Coordonnées</param>
 	/// <returns>bool</returns>
-	bool IsElementOverlap(const Vector2f& _pos) const;
-	bool GenerateRandomElement(const Vector2f& _pos);
-	void GenerateGround();
-	int GetRandomDirection();
-	void PlaceBarbedWire();
+	bool IsElementOverlap(const Vector2f& _pos) const
+	{
+		for (MeshActor* _actor : generatedElements)
+		{
+			if (_actor->GetMesh()->GetShape()->GetDrawable()->getGlobalBounds().contains(_pos)) return true;
+		}
+
+		return false;
+	}
+	bool GenerateRandomElement(const Vector2f& _pos)
+	{
+		if (IsElementOverlap(_pos)) return false;
+
+		LevelElement<T>* _element = GetRandomElement(elementsList);
+		if (!_element) return false;
+
+		MeshActor* _actor = Level::SpawnActor(_element->GetSubclass());
+		_actor->SetPosition(_pos);
+		_actor->SetOriginAtMiddle();
+		generatedElements.push_back(_actor);
+
+		return true;
+
+	}
+	void GenerateGround()
+	{
+		if (!ground) return;
+
+		for (u_int _y = 0; _y < sizeY; _y += groundTileSize.y)
+		{
+			for (u_int _x = 0; _x < sizeX; _x += groundTileSize.x)
+			{
+				LevelElement<T>* _groundElement = ground;
+				if (_groundElement->HasVariants())
+				{
+					_groundElement = ground->GetRandomVariant();
+				}
+				MeshActor* _ground = Level::SpawnActor(_groundElement->GetSubclass());
+				_ground->SetPosition(Vector2f((float)_x, (float)_y));
+				_ground->SetOriginAtMiddle();
+			}
+		}
+
+		PlaceBarbedWire();
+	}
+	int GetRandomDirection()
+	{
+		const vector<int> _directions =
+		{
+			0,
+			90,
+			180
+		};
+		const int _directionsCount = static_cast<u_short>(_directions.size());
+		return _directions[GetRandomNumberInRange(0, _directionsCount - 1)];
+	}
+	void PlaceBarbedWire()
+
+	{
+		vector<pair<string, CollisionType>> _responsesMesh = { { "Tank", CT_OVERLAP } };
+		float _barbedWireSize = groundTileSize.x;
+		// Placer les barbelés sur les bords
+		for (float _x = (-_barbedWireSize / 2); _x < sizeX /*+ _barbedWireSize*/; _x += _barbedWireSize)
+		{
+			MeshActor* _barbed1 = Level::SpawnActor(MeshActor(RectangleShapeData(Vector2f(_barbedWireSize, _barbedWireSize), "Object/Barbed")));
+			_barbed1->SetPosition(Vector2f(_x, -100));// Haut
+			_barbed1->SetOriginAtMiddle();
+
+			MeshActor* _barbed2 = Level::SpawnActor(MeshActor(RectangleShapeData(Vector2f(_barbedWireSize, _barbedWireSize), "Object/Barbed")));
+			_barbed2->SetOriginAtMiddle();
+			_barbed2->SetPosition(Vector2f(_x, sizeY - 30));// Bas
+		}
+
+		for (float _y = (-_barbedWireSize); _y < sizeY; _y += _barbedWireSize)
+		{
+			MeshActor* _barbed1 = Level::SpawnActor(MeshActor(RectangleShapeData(Vector2f(_barbedWireSize, _barbedWireSize), "Object/Barbed")));
+			_barbed1->SetPosition(Vector2f(-_barbedWireSize / 4, _y));// Gauche
+			_barbed1->Rotate(degrees(90));
+
+			MeshActor* _barbed2 = Level::SpawnActor(MeshActor(RectangleShapeData(Vector2f(_barbedWireSize, _barbedWireSize), "Object/Barbed")));
+			_barbed2->SetPosition(Vector2f(sizeX + _barbedWireSize / 4, _y));// Droite
+			_barbed2->Rotate(degrees(90));
+		}
+	}
 	friend LevelElement;
 
 	// On fait un set d'IDs pour pouvoir choisir aléatoirement un élément parmis la liste, histoire de ne pas les faire dans l'ordre du vecteur et maximiser le random
@@ -108,7 +266,7 @@ private:
 		return _variantsId;
 	}
 
-	static LevelElement* GetRandomElement(const vector<LevelElement*>& _vector)
+	static LevelElement* GetRandomElement(const vector<LevelElement<T>*>& _vector)
 	{
 		if (_vector.empty()) return nullptr;
 
